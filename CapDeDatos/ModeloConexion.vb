683395aa8da09bd1a90afd041dd001c8832ec602
gdicaterina <64912167+gdicaterina@users.noreply.github.com>
2020-08-07 12:26:36 -0300

    Ejemplo chat VB

---
+++ CapDeDatos/ModeloConexion.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.Odbc
+
+Public MustInherit Class ModeloConexion
+    Public DbDriver As String = "MySQL ODBC 8.0 ANSI Driver"
+    Public DbUser As String = "root"
+    Public DbPassword As String = "secreto"
+    Public DbPort As String = "3306"
+    Public DbName As String = "chat"
+    Public DbHost As String = "10.12.10.110"
+
+
+
+    Public Conexion As New OdbcConnection("DRIVER=" + Me.DbDriver +
+                                          ";UID=" + Me.DbUser +
+                                          ";PWD=" + Me.DbPassword +
+                                          ";PORT=" + Me.DbPort +
+                                          ";DATABASE=" + Me.DbName +
+                                          ";SERVER=" + Me.DbHost)
+
+
+    Public Comando As New OdbcCommand
+    Public Reader As OdbcDataReader
+
+
+    Public Sub New()
+        Me.Conexion.Open()
+        Me.Comando.Connection = Me.Conexion
+    End Sub
+
+
+
+End Class
---
+++ Chat/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports CapaLogica
+Imports System.Data.Odbc
+
+Public Class FormularioChat
+    Private Sub BtnEnviar_Click(sender As Object, e As EventArgs) Handles BtnEnviar.Click
+
+        ControladorMensaje.Guardar(txtSesion.Text, TxtId.Text, txtPara.Text, RTxtMensaje.Text)
+        RTxtChat.Text += Environment.NewLine + "YO:" + Environment.NewLine + RTxtMensaje.Text
+        WebBrowser1.DocumentText +=
+            "
+                <br />
+                <b>YO: </b> 
+                <br / >
+                " + RTxtMensaje.Text + " 
+            "
+    End Sub
+
+    Private Sub Timer1_Tick_1(sender As Object, e As EventArgs) Handles Pooling.Tick
+        'Dim lector As OdbcDataReader
+        Dim tabla As New DataTable
+        tabla = ControladorMensaje.BuscarMensajeNoLeido(txtSesion.Text, TxtId.Text)
+
+        'tabla.Load(ControladorMensaje.BuscarMensajeNoLeido(txtSesion.Text, TxtId.Text))
+
+        If tabla.Rows.Count > 0 Then
+
+            For Each fila As DataRow In tabla.Rows
+                RTxtChat.Text += Environment.NewLine + fila("emisor").ToString + " - " + fila("fechahora").ToString + Environment.NewLine + fila("texto").ToString
+
+                WebBrowser1.DocumentText +=
+            "
+                <br />
+                <b>" + fila("emisor") + " a las " + fila("fechahora") + " escribio: </b>
+                <br />
+                " + fila("texto") + " 
+                <br />            
+                "
+                ControladorMensaje.MarcarMensajeLeido(fila("id_mensaje").ToString)
+            Next
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Pooling.Enabled = True
+
+    End Sub
+
+    Private Sub FormularioChat_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
